9bed7c320c57b08e541fef541ba8e1f3d9682f92
Alejo Borracci <129368174+alejoborracci21@users.noreply.github.com>
2025-05-18 05:44:32 -0300

    first commit

---
+++ frontend/src/app/(auth)/login/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Login() {
+    return (
+        <div>
+        <h1>Login</h1>
+        <p>Welcome to the login page!</p>
+        </div>
+    );
+    }
---
+++ frontend/src/app/(protected)/questions/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Questions() {
+    return (
+        <div>
+        <h1>Questions</h1>
+        <p>Welcome to the questions page!</p>
+        </div>
+    );
+    }
